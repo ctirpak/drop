c168a136d49c5e7c910badb8944d7cf5fb84351c
Chris Tirpak <ctirpak1@gmail.com>
2021-06-09 12:29:57 -0400

    first working version

--- core/src/io/github/ctirpak/drop/Drop.java
+++ core/src/io/github/ctirpak/drop/Drop.java
@@ -1,5 +1,7 @@
 package io.github.ctirpak.drop;
 
+import java.util.Iterator;
+
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -8,10 +10,12 @@ import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.TimeUtils;
 
 public class Drop extends ApplicationAdapter {
 	private Texture dropImage;
@@ -22,63 +26,103 @@ public class Drop extends ApplicationAdapter {
 	private SpriteBatch batch;
 	private Rectangle bucket;
 	private Vector3 touchPos = new Vector3();
-	private Array<Rectangle>Drop raindrops;
-	
+	private Array<Rectangle> raindrops;
+	private Long lastDropTime;
+
 	@Override
-	public void create () {
+	public void create() {
 		// load the images for the droplet and the bucket, 64x64 pixels each
 		dropImage = new Texture(Gdx.files.internal("droplet.png"));
 		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
-		
+
 		// load the drop sound effect and the rain background music
 		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.mp3"));
 		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
-		
+
 		// start the playback of the background music immediately
 		rainMusic.setLooping(true);
 		rainMusic.play();
-		
+
 		// set up the camera
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false, 800, 400);
-		
+
 		batch = new SpriteBatch();
-		
+
 		bucket = new Rectangle();
 		bucket.x = 800 / 2 - 64 / 2;
 		bucket.y = 20;
 		bucket.width = 64;
 		bucket.height = 64;
+
+		raindrops = new Array<Rectangle>();
+		spawnRaindrop();
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		ScreenUtils.clear(0, 0, 0.2f, 1);
-		
+
 		// update camera
 		camera.update();
-		
+
 		// render images
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(bucketImage, bucket.x, bucket.y);
+		for (Rectangle raindrop: raindrops) {
+			batch.draw(dropImage, raindrop.x, raindrop.y);
+		}
 		batch.end();
-		
+
 		// get user input
-		if(Gdx.input.isTouched()) {
+		if (Gdx.input.isTouched()) {
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
 			bucket.x = touchPos.x - 64 / 2;
 		}
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
-		
-		if(bucket.x < 0) bucket.x = 0;
-		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT))
+			bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT))
+			bucket.x += 200 * Gdx.graphics.getDeltaTime();
+
+		if (bucket.x < 0)
+			bucket.x = 0;
+		if (bucket.x > 800 - 64)
+			bucket.x = 800 - 64;
+
+		// spawn raindrop
+		if (TimeUtils.nanoTime() - lastDropTime > 1000000000)
+			spawnRaindrop();
+
+		// move raindrops
+		for (Iterator<Rectangle> iter = raindrops.iterator(); iter.hasNext();) {
+			Rectangle raindrop = iter.next();
+			raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
+			if (raindrop.y + 64 < 0) iter.remove();
+			if (raindrop.overlaps(bucket)) {
+				dropSound.play();
+				iter.remove();
+			}
+		}
 	}
-	
+
+	private void spawnRaindrop() {
+		Rectangle raindrop = new Rectangle();
+		raindrop.x = MathUtils.random(0, 800 - 64);
+		raindrop.y = 480;
+		raindrop.width = 64;
+		raindrop.height = 64;
+		raindrops.add(raindrop);
+		lastDropTime = TimeUtils.nanoTime();
+	}
+
 	@Override
-	public void dispose () {
+	public void dispose() {
+		dropImage.dispose();
+		bucketImage.dispose();
+		dropSound.dispose();
+		rainMusic.dispose();
 		batch.dispose();
 	}
 }
