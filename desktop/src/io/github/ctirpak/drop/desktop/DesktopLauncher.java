93f586ab8b3a8a657025afad55201585e7161a51
Chris Tirpak <ctirpak1@gmail.com>
2021-06-10 09:24:52 -0400

    changed height

--- desktop/src/io/github/ctirpak/drop/desktop/DesktopLauncher.java
+++ desktop/src/io/github/ctirpak/drop/desktop/DesktopLauncher.java
@@ -9,7 +9,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Drop";
 		config.width = 800;
-		config.height = 480;
+		config.height = 600;
 		new LwjglApplication(new Drop(), config);
 	}
 }
